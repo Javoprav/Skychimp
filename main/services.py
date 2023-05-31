804cb99590d43c8c5b0d148fd3beaaf873d00f54
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-05-31 20:33:25 +0300

    ch 1

--- main/services.py
+++ main/services.py
@@ -11,16 +11,18 @@ def send_email():  # отправка письма
     for customer in Customer.objects.all():
         all_email.append(str(customer.email))
 
-    print(all_email)
+    # print(all_email)
     for send in Sending.objects.all():
         if send.status == 'Создана':
             message1 = send.message
             message = Message.objects.filter(subject=message1)
-            # print(message)
-            print(message)
-            # send_mail(
-            #     subject=message.subject,
-            #     message=message.body,
-            #     from_email=settings.EMAIL_HOST_USER,
-            #     recipient_list=[*all_email],
-            # )
+            for mes in message:
+                send_mail(
+                    subject=mes.subject,
+                    message=mes.body,
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[*all_email],
+                )
+            # создание статистики из модели Attempt
+
+            # создание рассылки по Sending.frequency
